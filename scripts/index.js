bb76463eaa44e22892018160d783dae19112f167
rezaulhasan <rezaulhassan50@gmail.com>
2025-10-04 11:17:22 +0200

    displayVideos Done

--- scripts/index.js
+++ scripts/index.js
@@ -30,28 +30,6 @@ function displayCategories(categories) {
         categoryContainer.append(categoryDiv);
     }
 }
-// {category_id: '1001', video_id: 'aaaa', thumbnail: 'https://i.ibb.co/L1b6xSq/shape.jpg', title: 'Shape of You', authors: Array(1), …}
-// authors
-// : 
-// [{…}]
-// category_id
-// : 
-// "1001"
-// description
-// : 
-// "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// others
-// : 
-// {views: '100K', posted_date: '16278'}
-// thumbnail
-// : 
-// "https://i.ibb.co/L1b6xSq/shape.jpg"
-// title
-// : 
-// "Shape of You"
-// video_id
-// : 
-// "aaaa"
 
    const displayVideos =(videos) =>{
 //  console.log(videos);
@@ -63,22 +41,22 @@ videos.forEach((video)=> {
     videoCard.innerHTML = `
                    <div class="card bg-base-100  ">
   <figure class="relative">
-    <img
-      src="https://i.ibb.co/QPNzYVy/moonlight.jpg" />
+    <img class="w-full h-[150px] object-cover "
+      src="${video.thumbnail}" />
       <span class="absolute bottom-2 right-2 text-sm rounded text-white bg-black px-2">3hrs 56 min ago</span>
   </figure>
   <div class=" flex gap-3 px-0 py-6">
     <div class="profile">
         <div class="avatar">
   <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring-2 ring-offset-2">
-    <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
+    <img src="${video.authors[0].profile_picture}" />
   </div>
 </div>
     </div>
     <div class="intro">
         <h2 class="text-sm font-semibold">Midnight Serenade</h2>
-        <p class="text-sm text-gray-400 flex gap-1">Awlad Hossain <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt=""></p>
-        <p class="text-sm text-gray-400">91k views</p>
+        <p class="text-sm text-gray-400 flex gap-1">${video.authors[0]. profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt=""></p>
+        <p class="text-sm text-gray-400">${video.others.views}</p>
     </div>
     </div>
   </div>
